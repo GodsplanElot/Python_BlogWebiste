beecf15b0a7b221af990b0af0d18e522c6d849b8
GodsplanELot <elotgodsplan2@gmail.com>
2024-12-07 17:20:08 +0100

    Changed the sorting oder of the post

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -3,5 +3,5 @@ from .models import post
 
 # Create your views here.
 def posts_list(request):
-    posts = post.objects.all()
+    posts = post.objects.all().order_by('-date')
     return render(request, 'posts/posts_list.html', {'posts':posts})
