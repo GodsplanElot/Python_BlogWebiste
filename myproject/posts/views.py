179a45a76049e9c8836103825e050e4ca23a1f1d
GodsplanELot <elotgodsplan2@gmail.com>
2024-12-07 16:52:13 +0100

    updated the post list template to recieve dynamic data

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from models import post
 
 # Create your views here.
 def posts_list(request):
-    return render(request, 'posts/posts_list.html')
+    posts = post.objects.all()
+    return render(request, 'posts/posts_list.html', {'posts':posts})
