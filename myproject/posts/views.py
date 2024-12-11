3df196647e115ea055e4a2279dea015f9d79eb6d
GodsplanELot <elotgodsplan2@gmail.com>
2024-12-11 07:11:45 +0100

    finished the logic in the views.py file in the post directory ☣

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import post 
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def posts_list(request):
@@ -10,5 +11,6 @@ def  post_page(request, slug):
     posts = post.objects.get(slug=slug)
     return render(request, 'posts/posts_page.html', {'post':posts})
 
+@login_required(login_url="/users/login/")
 def post_new(request):
     return render(request, 'posts/post_new.html')
