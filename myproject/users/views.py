150b538c5724cec5a728447fe088c4abbc2da63d
GodsplanELot <elotgodsplan2@gmail.com>
2024-12-11 07:39:47 +0100

    added feature in case secured page doesnt exist user can be redirected to the post list page ☣

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -20,7 +20,8 @@ def login_view(request):
             login(request, form.get_user())
             if 'next' in request.POST:
                 return redirect(request.POST.get('next'))
-            return redirect("posts:list")
+            else:
+                return redirect("posts:list")
     else:
         form = AuthenticationForm()
     return render(request, "users/login.html", {"form": form})
