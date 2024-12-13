3a6de30d7a98e26818ea00f114655f9cab7ec77d
GodsplanELot <elotgodsplan2@gmail.com>
2024-12-13 11:10:35 +0100

    Re-configured the code to upload to ubuntu VM, Cleaned up code for uchedotphp☣

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0+&&(pru^okv-ecnk*p&0vwxr_+_96jxcvk+h=^6-+)=xgq5qo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = ["localhost","127.0.0.1"]
 
 
 # Application definition
